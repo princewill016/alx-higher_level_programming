4e993daf27e9b2985bfb69dee4375455241550b7
Peter david <princewill016@gmail.com>
2024-09-10 05:05:20 -0700

    done with all tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print("".join("{}".format(chr(c)) for c in range(97, 123) if c != 113 and c != 101), end="")
+print("".join(
+    "{}".format(chr(c))
+    for c in range(97, 123)
+    if c != 113 and c != 101
+), end="")
