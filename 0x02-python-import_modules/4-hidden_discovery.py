5c034702776b8d23d3568553c32a0f6415e9f923
Peter david <princewill016@gmail.com>
2024-09-13 00:17:52 -0700

    100

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
 
-names = dir(hidden_4)
-for name in sorted(names):
-    if not name.startswith("__"):
+if __name__ == "__main__":
+    import hidden_4
+
+    names = dir(hidden_4)
+
+    filtered_names = sorted(name for name in names if not name.startswith("__"))
+
+    for name in filtered_names:
         print(name)
