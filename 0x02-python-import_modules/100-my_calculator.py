d1d86267dc0b154881621e3a060efe7cd9272882
Peter david <princewill016@gmail.com>
2024-09-13 00:05:54 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,32 +2,26 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-if len(sys.argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
 
-try:
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
-except ValueError:
-    print("Error: Arguments must be integers")
-    sys.exit(1)
 
-operators = {
-    '+': add,
-    '-': sub,
-    '*': mul,
-    '/': div
-}
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if operator not in operators:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
-
-if operator == '/' and b == 0:
-    print("Error: Division by zero")
-    sys.exit(1)
-
-result = operators[operator](a, b)
-print(f"{a} {operator} {b} = {result}")
+    print("{} {} {} = {}".format(a, operator, b, result))
