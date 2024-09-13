b6a30281766582d918dca8c98640d208e8934675
Peter david <princewill016@gmail.com>
2024-09-13 00:21:47 -0700

    100

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-import sys
 
-def sum_arguments():
-    total = sum(int(arg) for arg in sys.argv[1:])
-    print(total)
+if __name__ == "__main__":
+    from sys import argv
+
+    result = sum(int(arg) for arg in argv[1:])
+
+    print(result)
+
