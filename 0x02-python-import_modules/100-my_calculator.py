bb1e72b17753dccb885b4c462fe5939a1fb9ad7b
Peter david <princewill016@gmail.com>
2024-09-13 00:03:53 -0700

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,14 +2,17 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
-args = len(sys.argv)
-if args != 4:
+if len(sys.argv) != 4:
     print("Usage: ./100-my_calculator.py <a> <operator> <b>")
     sys.exit(1)
 
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
+try:
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+except ValueError:
+    print("Error: Arguments must be integers")
+    sys.exit(1)
 
 operators = {
     '+': add,
@@ -20,6 +23,10 @@ operators = {
 
 if operator not in operators:
     print("Unknown operator. Available operators: +, -, * and /")
+    sys.exit(1)
+
+if operator == '/' and b == 0:
+    print("Error: Division by zero")
     sys.exit(1)
 
 result = operators[operator](a, b)
