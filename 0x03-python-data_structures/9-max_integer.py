90f1ed438ecb9a743c8e584c5f74b76c692431ab
Peter david <princewill016@gmail.com>
2024-09-14 01:03:10 -0700

    vvv

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
+    if len(my_list) == 0:
         return None
-
-    largest = my_list[0]
-
-    for num in my_list:
-        if num > largest:
-            largest = num
     
-    return largest
+    biggest = my_list[0]
+    for num in my_list[1:]:
+        if num > biggest:
+            biggest = num
+    
+    return biggest
