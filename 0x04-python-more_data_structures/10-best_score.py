71f0993c1dc3aa5165559e6ab926f4c19e02f9ab
Peter david <princewill016@gmail.com>
2024-09-20 21:48:28 -0700

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    """
-    Return the key with the biggest integer value in a dictionary.
-
-    Args:
-        a_dictionary (dict): The dictionary with integer values.
-
-    Returns:
-        str: The key with the highest value, or None if the dictionary is empty.
-    """
     if not a_dictionary:
         return None
     return max(a_dictionary, key=a_dictionary.get)
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    """Return a new matrix with each value squared."""
-    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
