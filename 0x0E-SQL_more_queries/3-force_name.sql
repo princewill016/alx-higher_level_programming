3005b9acd2f7c69316b5edcc7d7dc2868c890bda
Peter david <princewill016@gmail.com>
2024-11-20 21:23:39 -0800

    done

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,6 @@
--- 3-create_table.sql
--- Script to create the table 'force_name' with specified columns in the given database
-
--- Create the table if it does not already exist
+-- Create force_name table with specified constraints
+-- Ensures table creation doesn't fail if it already exists
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
-
