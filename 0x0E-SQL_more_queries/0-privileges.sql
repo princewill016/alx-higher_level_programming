b80c3f848b516de95167bc8e4284d2bb9f0fdc9c
Peter david <princewill016@gmail.com>
2024-11-20 20:56:21 -0800

    done

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- 0-privileges.sql
+-- Script to list all privileges of users 'user_0d_1' and 'user_0d_2' on localhost
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
